cde6acd08559f250f7641f7b83836abfe7a97d75
Arif Rahman <matrep88@gmail.com>
2022-05-23 13:20:59 +0800

    test import

--- src/hooks/useWhitelistMint.ts
+++ src/hooks/useWhitelistMint.ts
@@ -1,7 +1,7 @@
 import { useToast } from "@chakra-ui/react";
 import { useNetwork, useContractWrite } from "wagmi";
 import { CONTRACT_ADDRESS } from "constants/constants";
-import foxfoneContract from "../artifacts/contracts/KitsudenFoxfone.sol/KitsudenFoxfone.json";
+import foxfoneContract from "../artifacts/contracts/KitsudenFoxfone/KitsudenFoxfone.json";
 import useCheckIsAddressWhiteListed from "./useCheckIsAddressWhiteListed";
 import { BigNumber } from "ethers";
 
