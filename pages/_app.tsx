4e4f0d158d5f4448d2ff860e003397860673ba1a
Arif Rahman <matrep88@gmail.com>
2022-11-04 00:21:35 +0800

    add: tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { wagmiClient } from "config/wagmi";
 import { ConnectKitProvider } from "connectkit";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { theme } from "styles";
 import { WagmiConfig } from "wagmi";
@@ -10,6 +11,23 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <WagmiConfig client={wagmiClient}>
       <ConnectKitProvider>
         <ChakraProvider theme={theme}>
+          <DefaultSeo
+            openGraph={{
+              type: "website",
+              locale: "en_IE",
+              url: "https://kitsuden.com/",
+              siteName: "Kitsuden",
+              images: [
+                {
+                  url: "https://www.superful.xyz/_next/image?url=https%3A%2F%2Fsuperful-assets-prod.s3.amazonaws.com%2Fimages%2F9ed4e498-ebc5-4568-845a-0b7736f159e0.png&w=828&q=75",
+                  alt: "kitsuden alt",
+                },
+              ],
+            }}
+            twitter={{
+              cardType: "summary_large_image",
+            }}
+          />
           <Component {...pageProps} />
         </ChakraProvider>
       </ConnectKitProvider>
