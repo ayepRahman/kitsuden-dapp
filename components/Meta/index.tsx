9305e8f5675626744cad47d7e6874c390fd649ea
Arif Rahman <matrep88@gmail.com>
2022-11-04 13:39:18 +0800

    test

--- components/Meta/index.tsx
+++ components/Meta/index.tsx
@@ -55,44 +55,62 @@ export const Meta: React.FC<{
   description = "Kitsuden is an NFT project that has multiple layers (Chapters) tied to its journey. For the first chapter, We are releasing 6,666 NFTs with 3 tiers of rarity.",
 }) => {
   return (
+    // <Head>
+    //   <title>{title}</title>
+    //   <link rel="icon" href="/favicon.ico" />
+
+    //   {/* <!-- Basic --> */}
+    //   <meta name="viewport" content="width=device-width, initial-scale=1" />
+    //   <meta name="theme-color" content="#1f1860" />
+    //   <meta httpEquiv="Cache-Control" content="0" />
+    //   <meta name="keywords" content={keywords} />
+    //   <meta name="description" content={description} />
+    //   <meta charSet="utf-8" />
+
+    //   {/* <!--  OG --> */}
+    //   <meta property="og:title" content={title} />
+    //   <meta property="og:type" content="website" />
+    //   <meta
+    //     property="og:image"
+    //     content="https://superful-assets-prod.s3.amazonaws.com/images/9ed4e498-ebc5-4568-845a-0b7736f159e0.png"
+    //   />
+    //   <meta property="og:url" content="http://kitsuden.com" />
+    //   <meta property="og:description" content={description} />
+    //   <meta property="og:site_name" content="Kitsuden" />
+    //   <meta property="og:locale" content="en_US" />
+
+    //   {/* <!-- twitter --> */}
+    //   <meta name="twitter:title" content="Kitsuden" />
+    //   <meta property="twitter:card" content="summary_large_image" />
+    //   <meta name="twitter:description" content={description} />
+    //   <meta
+    //     property="twitter:image"
+    //     content="https://superful-assets-prod.s3.amazonaws.com/images/9ed4e498-ebc5-4568-845a-0b7736f159e0.png"
+    //   />
+    //   <meta name="twitter:image:alt" content="Kitsuden" />
+    //   <meta name="twitter:site" content="@KitsudenNFT" />
+
+    //   <meta name="apple-mobile-web-app-title" content="Kitsuden" />
+    //   <meta name="application-name" content="Kitsuden" />
+    //   <meta name="author" content="Kitsuden" />
+    // </Head>
     <Head>
-      <title>{title}</title>
-      <link rel="icon" href="/favicon.ico" />
-
-      {/* <!-- Basic --> */}
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#1f1860" />
-      <meta httpEquiv="Cache-Control" content="0" />
-      <meta name="keywords" content={keywords} />
-      <meta name="description" content={description} />
-      <meta charSet="utf-8" />
-
-      {/* <!--  OG --> */}
-      <meta property="og:title" content={title} />
-      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@nytimesbits" />
+      <meta name="twitter:creator" content="@nickbilton" />
+      <meta
+        property="og:url"
+        content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/"
+      />
+      <meta property="og:title" content="A Twitter for My Sister" />
+      <meta
+        property="og:description"
+        content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling."
+      />
       <meta
         property="og:image"
-        content="https://superful-assets-prod.s3.amazonaws.com/images/9ed4e498-ebc5-4568-845a-0b7736f159e0.png"
+        content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
       />
-      <meta property="og:url" content="http://kitsuden.com" />
-      <meta property="og:description" content={description} />
-      <meta property="og:site_name" content="Kitsuden" />
-      <meta property="og:locale" content="en_US" />
-
-      {/* <!-- twitter --> */}
-      <meta name="twitter:title" content="Kitsuden" />
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta name="twitter:description" content={description} />
-      <meta
-        property="twitter:image"
-        content="https://superful-assets-prod.s3.amazonaws.com/images/9ed4e498-ebc5-4568-845a-0b7736f159e0.png"
-      />
-      <meta name="twitter:image:alt" content="Kitsuden" />
-      <meta name="twitter:site" content="@KitsudenNFT" />
-
-      <meta name="apple-mobile-web-app-title" content="Kitsuden" />
-      <meta name="application-name" content="Kitsuden" />
-      <meta name="author" content="Kitsuden" />
     </Head>
   );
 };
