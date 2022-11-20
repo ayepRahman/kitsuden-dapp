e56e885d4efada94db955ffb654b33961fc69734
Arif Rahman <matrep88@gmail.com>
2022-11-20 22:59:16 +0800

    added new section

--- components/Home/Lore.tsx
+++ components/Home/Lore.tsx
@@ -4,6 +4,7 @@ import ChakraText from "components/ChakraText";
 import Image from "components/Image";
 import { motion, useAnimation } from "framer-motion";
 import loreBg from "public/img/bg_section_1.png";
+import loreBrushBg2 from "public/img/lore_bg-2.png";
 import loreBrushBg from "public/img/lore_brush_bg.png";
 import React from "react";
 import { useInView } from "react-intersection-observer";
@@ -16,15 +17,32 @@ const lines = {
   four: "As it beckons to you..are you brave enough to follow it?",
 };
 
+const lines2 = {
+  one: "The adventure picks up right when a group of adventurers decided to follow the signal coming from the broken Foxfone. Riches and greatness only await within the ever-mentioned hidden kitsune village.",
+  two: "Along their adventures, they started finding ancient caches, and to their surprise inside are Foxfones! The quantity ranges from the cache to cache but there were always a few of them stored. Groups quickly discovered that there are different types of Foxfones, Each with different colors and shapes. And sure enough, the Foxfones are working with different programs running in them, But somehow most of them are locked. Only the radar and map are available.",
+  three:
+    "This is it, the way to find the hidden mist village, Kitsuden. What started as a spur-of-the-moment adventure ignited by the discovery of a broken ancient technology called Foxfones, Quickly became a hunt to gather ancient caches in hopes of getting working Foxfones to guide the adventurers on their journey.",
+  four: "Will you find one of these caches spread throughout the journey and get yourself and your group Foxfones?",
+};
+
 const Lore = React.forwardRef((_, ref) => {
-  const [boxRef, inView] = useInView();
-  const refs = useMergeRefs(ref, boxRef);
+  const [boxRefOne, inView] = useInView();
+  const [boxRefTwo, inViewTwo] = useInView();
+  const refs = useMergeRefs(ref, boxRefOne);
+  // 1
   const controlsHeader = useAnimation();
   const controlsImg = useAnimation();
   const controlsLineOne = useAnimation();
   const controlsLineTwo = useAnimation();
   const controlsLineThree = useAnimation();
   const controlsLineFour = useAnimation();
+  // 2
+  const controlsHeader2 = useAnimation();
+  const controlsImg2 = useAnimation();
+  const controlsLineOne2 = useAnimation();
+  const controlsLineTwo2 = useAnimation();
+  const controlsLineThree2 = useAnimation();
+  const controlsLineFour2 = useAnimation();
 
   const headerVariants = {
     hidden: { opacity: 0, x: 100 },
@@ -42,7 +60,7 @@ const Lore = React.forwardRef((_, ref) => {
       opacity: 1,
       transition: {
         // delay: 0.3,
-        staggerChildren: 0.02,
+        staggerChildren: 0.01,
       },
     },
   };
@@ -60,6 +78,14 @@ const Lore = React.forwardRef((_, ref) => {
       controlsHeader.start("visible");
       controlsImg.start("visible");
       controlsLineOne.start("visible");
+    }
+  }, [inView]);
+
+  React.useEffect(() => {
+    if (inViewTwo) {
+      controlsHeader2.start("visible");
+      controlsImg2.start("visible");
+      controlsLineOne2.start("visible");
     }
   }, [inView]);
 
@@ -94,22 +120,25 @@ const Lore = React.forwardRef((_, ref) => {
         />
       </Box>
 
+      {/* top */}
       <Container
         ref={refs}
         position="relative"
         zIndex={1}
         maxW="1440px"
         background="Background.100"
+        marginTop={["7rem"]}
+        marginBottom={["4rem", "4rem", "4rem", "14.5rem"]}
       >
         <Flex
-          gap="2rem"
+          gap="70px"
           alignItems="center"
           flexWrap={["wrap", "wrap", "wrap", "nowrap"]}
         >
           <ChakraBox
-            flex={[`0 0 100%`, `0 0 100%`, `0 0 100%`, `0 0 50%`]}
-            width={["100%", "748px"]}
-            height={["400px", "748px"]}
+            flex={[`0 0 100%`, `0 0 100%`, `0 0 100%`, `0 0 798px`]}
+            width={["100%", "798px"]}
+            height={["400px", "798px"]}
             position="relative"
             initial="hidden"
             animate={controlsImg}
@@ -243,6 +272,160 @@ const Lore = React.forwardRef((_, ref) => {
               })}{" "}
             </ChakraText>
           </Box>
+        </Flex>
+      </Container>
+      {/* bottom */}
+      <Container
+        ref={boxRefTwo}
+        position="relative"
+        zIndex={1}
+        maxW="1440px"
+        background="Background.100"
+        marginBottom={["7rem"]}
+      >
+        <Flex
+          gap="70px"
+          alignItems="center"
+          flexWrap={["wrap", "wrap", "wrap", "nowrap"]}
+          // flexDir={["revert"]}
+        >
+          <Box width={["100%", "100%", "100%", "50%"]}>
+            <ChakraBox
+              initial="hidden"
+              animate={controlsHeader2}
+              // @ts-ignore
+              transition={{
+                duration: 1.5,
+              }}
+              variants={headerVariants}
+            >
+              <Heading
+                color="black"
+                fontWeight={400}
+                fontSize={[46, 84]}
+                lineHeight={["30px", "76px"]}
+              >
+                an Ever evolving
+              </Heading>
+            </ChakraBox>
+            <ChakraBox
+              initial="hidden"
+              animate={controlsHeader2}
+              // @ts-ignore
+              transition={{
+                duration: 1.5,
+                delay: 0.3,
+              }}
+              variants={headerVariants}
+            >
+              <Heading
+                color="black"
+                fontWeight={400}
+                fontSize={[46, 84]}
+                lineHeight={["30px", "76px"]}
+                textAlign="right"
+              >
+                adventure
+              </Heading>
+            </ChakraBox>
+            <ChakraText
+              color="black"
+              lineHeight="160%"
+              my="2rem"
+              fontSize={[16, 18]}
+              fontWeight="600"
+              variants={sentenceVariants}
+              initial="hidden"
+              animate={controlsLineOne2}
+              onAnimationComplete={() => controlsLineTwo2.start("visible")}
+            >
+              {lines2.one.split("").map((char, i) => {
+                return (
+                  <motion.span variants={letterVariants} key={`${char}-${i}`}>
+                    {char}
+                  </motion.span>
+                );
+              })}
+            </ChakraText>
+            <ChakraText
+              color="black"
+              lineHeight="160%"
+              my="2rem"
+              fontSize={[16, 18]}
+              fontWeight="600"
+              variants={sentenceVariants}
+              initial="hidden"
+              animate={controlsLineTwo2}
+              onAnimationComplete={() => controlsLineThree2.start("visible")}
+            >
+              {lines2.two.split("").map((char, i) => {
+                return (
+                  <motion.span variants={letterVariants} key={`${char}-${i}`}>
+                    {char}
+                  </motion.span>
+                );
+              })}
+            </ChakraText>
+            <ChakraText
+              color="black"
+              lineHeight="160%"
+              my="2rem"
+              fontSize={[16, 18]}
+              fontWeight="600"
+              variants={sentenceVariants}
+              initial="hidden"
+              animate={controlsLineThree2}
+              onAnimationComplete={() => controlsLineFour2.start("visible")}
+            >
+              {lines2.three.split("").map((char, i) => {
+                return (
+                  <motion.span variants={letterVariants} key={`${char}-${i}`}>
+                    {char}
+                  </motion.span>
+                );
+              })}
+            </ChakraText>
+            <ChakraText
+              color="black"
+              lineHeight="160%"
+              my="2rem"
+              fontSize={[16, 18]}
+              fontWeight="600"
+              variants={sentenceVariants}
+              initial="hidden"
+              animate={controlsLineFour2}
+            >
+              {lines2.four.split("").map((char, i) => {
+                return (
+                  <motion.span variants={letterVariants} key={`${char}-${i}`}>
+                    {char}
+                  </motion.span>
+                );
+              })}{" "}
+            </ChakraText>
+          </Box>
+
+          <ChakraBox
+            flex={[`0 0 100%`, `0 0 100%`, `0 0 100%`, `0 0 798px`]}
+            width={["100%", "798px"]}
+            height={["400px", "798px"]}
+            position="relative"
+            initial="hidden"
+            animate={controlsImg2}
+            variants={loreBrushBgVariants}
+            // @ts-ignore
+            transition={{
+              duration: 1.5,
+            }}
+          >
+            <Image
+              alt="loreBrushBg"
+              priority
+              src={loreBrushBg2}
+              layout="fill"
+              placeholder="empty"
+            />
+          </ChakraBox>
         </Flex>
       </Container>
     </Box>
