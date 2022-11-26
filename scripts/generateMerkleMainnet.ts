c2e384019432cf5d05427d17fe7b1697a4a41a6f
Arif Rahman <matrep88@gmail.com>
2022-11-26 19:32:10 +0800

    added gefi fren

--- scripts/generateMerkleMainnet.ts
+++ scripts/generateMerkleMainnet.ts
@@ -1,4 +1,7 @@
 import { WHITE_LIST_ADDRESSES } from "../constants/constants";
 import { generateMerkle } from "../utils/merkle";
 
-console.log("PROD MERKLE ROOT", generateMerkle(WHITE_LIST_ADDRESSES[1]));
+console.log(
+  "PROD MERKLE ROOT",
+  generateMerkle(WHITE_LIST_ADDRESSES[1])?.merkleRootHash
+);
