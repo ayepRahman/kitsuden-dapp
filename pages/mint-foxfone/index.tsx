72f80d787a4588edfb2cc7f89ad30c9624bf0d5e
Arif Rahman <matrep88@gmail.com>
2022-11-26 20:37:08 +0800

    minting page lalal

--- pages/mint-foxfone/index.tsx
+++ pages/mint-foxfone/index.tsx
@@ -1,9 +1,11 @@
-import { Box, Container, Flex, Image } from "@chakra-ui/react";
+import { AspectRatio, Box, Container, Flex } from "@chakra-ui/react";
 import NetworkDetectorBanner from "@components/NetworkDetectorBanner";
 import { Meta } from "components/Meta";
 import Header from "containers/Header";
 import dynamic from "next/dynamic";
-import mintFoxfoneBg from "public/img/mint_foxfone_bg.png";
+
+const MINTING_VIDEO =
+  "https://kitsuden.s3.amazonaws.com/images/kitsuden-foxfone-mint.mp4";
 
 const Minting = dynamic(() => import("containers/Minting"), { ssr: false });
 const MintModelViewer = dynamic(() => import("components/MintModelViewer"), {
@@ -17,20 +19,20 @@ const Mint = () => {
       <Box h="full">
         <NetworkDetectorBanner />
         <Header />
-        <Box position="relative" h="full">
-          <Image
-            alt="mintFoxfoneBg"
-            position="absolute"
-            src={mintFoxfoneBg.src}
-            height={["100%", "100vh"]}
-            width="100%"
-            objectFit={"cover"}
-            bgPos="top"
+        <Box position="relative" h={["100%", "100vh"]}>
+          <AspectRatio
+            ratio={[9 / 16, 16 / 9]}
             zIndex="-1"
-            placeholder="empty"
-            opacity="70%"
-          />
-          <Container h="full" maxW={1600} py="5rem">
+            position="absolute"
+            top="0"
+            right="0"
+            bottom="0"
+            left="0"
+          >
+            <video muted autoPlay loop src={MINTING_VIDEO} />
+          </AspectRatio>
+
+          <Container h="full" maxW={1600} py="5rem" zIndex={2}>
             <Flex
               flexWrap={["wrap-reverse", "nowrap"]}
               gap="5rem"
